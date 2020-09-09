3e4530fe66a26c404eca32d775b6afbf32b431f2
tami-sub <45237530+tami-sub@users.noreply.github.com>
2020-09-09 21:31:11 +0300

    Update lab_1.py

--- lab_1/lab_1.py
+++ lab_1/lab_1.py
@@ -1,27 +1,31 @@
-n = float(input())
-f = open("test.txt", 'w+')
-"""Открыт или создан файл test.txt в зависимотсти от существования файла с таким названием"""
-lst = []
-"""Создан список lst"""
-
-def list_filling(n):
-    k = 0
-    while k < n:
-        lst.append(float(input()))
-        k = k + 1
-"""Заполнение списка"""
+f = open("test.txt", 'w+')  # Открыт или создан файл test.txt в зависимотсти от существования файла с таким названием 
+# Достоинства txt: -простота - экономичность
 
 
-def list_changing(local_list):
+def list_filling():  # Ввод значений через пробел, а затем изменение типа элементов списка
+    temp = input().split(' ')
+    temp = [float(x) for x in temp]
+    return temp
+
+
+def list_changing(local_list):  # Умножение всех значений списка на 0.13 и сортировка
     local_list = [x * 0.13 for x in local_list]
     local_list.sort()
     return local_list
-"""Умножение всех значений списка на 0.13 и сортировка"""
 
-list_filling(n)
+
+def list_rounding(local_list):  # Округление элементов списка до двух знаков
+    return [round(x, 2) for x in local_list]
+
+
+def list_writing(local_list):  # Запись значений в файл
+    for x in local_list:
+        f.write(str(x) + "\n")
+
+
+lst = list_filling()
 lst = list_changing(lst)
-for x in lst:
-    f.write(str(round(x, 2)) + "\n")
-"""Вывод с округлением до 2-х цифр"""
-f.close()
-"""Файл закрыт"""
+lst = list_rounding(lst)
+list_writing(lst)
+
+f.close()  # Закрытие файла на запись
